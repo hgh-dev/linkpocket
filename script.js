d9cdddbb97c5b72c6c9772aae2e3cc2f5a77917a
HeoGwangho <hussell.app@gmail.com>
2026-02-09 00:48:52 +0900

    v1.1.1

--- script.js
+++ script.js
@@ -399,7 +399,14 @@ function toggleFolderOrderMode() {
         btn.classList.add('active');
         sidebar.classList.add('edit-mode'); // CSS 스타일 적용용 클래스
     } else {
-        btn.innerText = '순서';
+        // 아이콘과 텍스트 복구
+        btn.innerHTML = `
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none"
+                stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path>
+                <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path>
+            </svg>
+            폴더 수정`;
         btn.classList.remove('active');
         sidebar.classList.remove('edit-mode');
     }
